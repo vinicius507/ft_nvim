82ff708e70420ed8f087c23565592c9aba213b1c
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-07-03 16:54:04 -0300

    chore: nix flake init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    allSystems = ["x86_64-linux" "aarch64-linux"];
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = builtins.attrValues self.overlays;
+          };
+        });
+  in {
+    packages = forEachSystem (
+      {pkgs}: {
+        lib = {
+          inherit forEachSystem;
+        };
+      }
+    );
+  };
+}
