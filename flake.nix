b606a5df52d15a8e842ac1475994162c1a2a1b76
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-07-11 21:34:39 -0300

    chore(nix): Adds devShell with norminette package

--- flake.nix
+++ flake.nix
@@ -17,20 +17,28 @@
           };
         });
   in {
-    packages = forEachSystem (
-      {pkgs}: {
-        lib = {
-          inherit forEachSystem;
-        };
-        ft_nvim = import ./nix/pkgs/ft_nvim.nix {
-          stdenv = pkgs.stdenvNoCC;
-        };
-      }
-    );
+    lib = forEachSystem ({pkgs}: {
+      inherit forEachSystem;
+      mkShell = pkgs.mkShell.override {
+        inherit (pkgs.llvmPackages_12) stdenv;
+      };
+    });
+    packages = forEachSystem ({pkgs}: {
+      ft_nvim = import ./nix/pkgs/ft_nvim.nix {
+        stdenv = pkgs.stdenvNoCC;
+      };
+    });
     overlays = {
       default = final: _: {
         ft_nvim = self.packages.${final.system}.ft_nvim;
       };
     };
+    devShells = forEachSystem ({pkgs}: {
+      default = self.lib.${pkgs.system}.mkShell {
+        packages = with pkgs; [
+          norminette
+        ];
+      };
+    });
   };
 }
