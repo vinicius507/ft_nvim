3d9595f994ed1731d4b897b0e930d9bd5b42bc42
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-07-16 15:50:34 -0300

    fix: Improves setup typing and validation

--- lua/ft_nvim/init.lua
+++ lua/ft_nvim/init.lua
@@ -5,9 +5,27 @@
 local header = require("ft_nvim.header")
 local norminette = require("ft_nvim.norminette")
 
+---@type fun(opts: unknown): ft_nvim.Config
+local function validate_config(opts)
+	if not opts then
+		return {}
+	end
+
+	vim.validate({
+		header = { opts.header, "table", true },
+		norminette = { opts.norminette, "table", true },
+	})
+	return {
+		header = opts.header,
+		norminette = opts.norminette,
+	}
+end
+
 return {
-	---@param opts ft_nvim.Config
+	---@param opts? ft_nvim.Config
 	setup = function(opts)
+		opts = validate_config(opts)
+
 		header.setup(opts.header)
 		norminette.setup(opts.norminette)
 	end,
